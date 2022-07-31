553e5098608386b44c44ff38e9903fdc8243c8c7
Helena2018 <helena2020liu@gmail.com>
2022-07-31 19:42:35 +1200

    use array and map func

--- src/App.js
+++ src/App.js
@@ -7,23 +7,25 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monster1: {
-        name: 'Frank'
-      },
-      monster2: {
-        name: 'Jacky'
-      },
-      monster3: {
-        name: 'Linda'
-      },
+      monsters: [
+        {
+          name: 'Franky'
+        },
+        {
+          name: 'Jacky'
+        },
+        {
+          name: 'Anny'
+        },
+      ]
     };
   }
   render() {
     return (
     <div className="App">
-      <h1>{this.state.monster1.name}</h1>
-      <h1>{this.state.monster2.name}</h1>
-      <h1>{this.state.monster3.name}</h1>
+      {this.state.monsters.map((monster) => {
+        return <h1>{monster.name}</h1>
+      })}
     </div>);
   }
 }
