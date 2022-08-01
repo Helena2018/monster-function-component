5c24c4876f211f5aa410f22ca9db992086f3e714
Helena2018 <helena2020liu@gmail.com>
2022-08-01 21:30:04 +1200

    degub monster's'

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
           })
 
           this.setState(() => {
-            return { monster: filteredMonsters}
+            return { monsters: filteredMonsters}
           })
         }} 
       />
