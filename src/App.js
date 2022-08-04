4856bd1212399f9ba7b042c587caf620f031822d
Helena2018 <helena2020liu@gmail.com>
2022-08-04 21:12:04 +1200

    clear code

--- src/App.js
+++ src/App.js
@@ -31,8 +31,12 @@ class App extends Component {
   }
 
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
@@ -41,7 +45,7 @@ class App extends Component {
         className='search-box' 
         type='search' 
         placeholder='Search Monster' 
-        onChange={ this.onSearchChange } 
+        onChange={ onSearchChange } 
       />
       {filteredMonsters.map((monster) => {
         return (
