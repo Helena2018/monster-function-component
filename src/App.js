b8af84553812777ba2948f7a7cc2732177fc5c83
Helena2018 <helena2020liu@gmail.com>
2022-08-05 17:21:10 +1200

    render page

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ class App extends Component {
           placeholder='Search Monster' 
           onChange={ onSearchChange } 
       />
-      <CardList monster={ filteredMonsters}/>
+      <CardList monsters={filteredMonsters}/>
     </div>
     );
   }
