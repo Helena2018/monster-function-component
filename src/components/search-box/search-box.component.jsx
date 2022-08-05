63a81f41cd2c1df614c9657a5ddcb31f158f2c91
Helena2018 <helena2020liu@gmail.com>
2022-08-05 21:09:06 +1200

    SearchBox props

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -4,9 +4,9 @@ class SearchBox extends Component {
   render() {
     return (
       <input 
-          // className='search-box' 
+          className={ this.props.className }
           type='search' 
-          // placeholder='Search Monster' 
+          placeholder={ this.props.placeholder }
           onChange={ this.props.onChangeHandler } 
       />
     );
