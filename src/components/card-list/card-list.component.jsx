14c4985d43ac6af13666475d32c2d491ab7c67a7
Helena2018 <helena2020liu@gmail.com>
2022-08-06 14:29:50 +1200

    destructure coding

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -4,17 +4,19 @@ class CardList extends Component {
   render() {
     const { monsters } = this.props;
     return (
-      <div>
-        {monsters.map((monster) => (
-          <div className='card-container' key={monster.id}>
+      <div className='card-list'>
+        {monsters.map((monster) => {
+          const { id, name, email } = monster
+          return (
+          <div className='card-container' key={id}>
             <img
               alt={`monster ${monster.name}`}
               src={`https://robohash.org/${monster.id}?set=set3&size=180x180`}
             />
-            <h2>{monster.name}</h2>
-            <p>{monster.email}</p>
+            <h2>{name}</h2>
+            <p>{email}</p>
           </div>
-        ))}
+        )})}
       </div>
     );
   }
