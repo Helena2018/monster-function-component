44b903fdaf0778224900d7a03bdebe94b9963157
Helena2018 <helena2020liu@gmail.com>
2022-08-08 13:58:27 +1200

    new filter monster

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import SearchBox from './components/search-box/search-box.component';
 const App = () => {
   const [searchField, setSearchField] = useState('');
   const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
 
   console.log('render')
 
@@ -16,16 +17,22 @@ const App = () => {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => setMonsters(users));
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   };  
 
-  const filteredMonsters = monsters.filter((monster) => {
-    return monster.name.toLocaleLowerCase().includes(searchField);
-  });
+
 
   return (
     <div className="App">
